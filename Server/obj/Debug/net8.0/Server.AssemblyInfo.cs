578f8d7ad37dca3a919d3e44c30304e2256d90fa
Nils Edvardsson <nils@nilsjc.com>
2025-01-09 15:17:03 +0100

    moved ignore file. Created new web api project

--- Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
+++ Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa39234fc0cdd70bb3dbdf6ce6789046552a86f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a759d9054c630cc8eab88fe10a054f8906284ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
