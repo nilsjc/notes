5a759d9054c630cc8eab88fe10a054f8906284ee
Nils Edvardsson <nils@nilsjc.com>
2025-01-09 15:02:23 +0100

    added ignore file

--- Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
+++ Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+927783ee897d21a45b80504496e4e07042c5a847")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa39234fc0cdd70bb3dbdf6ce6789046552a86f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
